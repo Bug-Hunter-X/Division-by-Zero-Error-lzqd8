b678a26a5bdb0c23b0e68ce9131d25c64d57b5dc
BugHunter-X <askzenai@gmail.com>
2024-12-26 13:06:04 +0100

    Add bugSolution.c file

---
+++ bugSolution.c
@@ -0,0 +1,3 @@
+int main() { int a = 10; int b = 0; if (b == 0) { printf("Error: Division by zero!
+"); return 1; // Indicate an error } else { int c = a / b; printf("Result: %d
+", c); } return 0; }
